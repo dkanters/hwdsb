fbb3372844eb3359dc05e6e519f1332d6b79e52b
David Kanters <kanters.david@gmail.com>
2023-08-18 13:14:32 -0400

    always write a commmit message to briefly describe what you did

--- R/test.R
+++ R/test.R
@@ -1,4 +1,13 @@
 
+#' test
+#'
+#' @param x a character vector
+#'
+#' @return a vector of numeric values
+#' @export
+#'
+#' @examples
+#' #test(x)
 test <- function(x) {
   suppressWarnings(
     x <- as.numeric(x)
